ed8098f42b64b4dbbcf09b98a590d501de2903d8
DejaVu <deja-vu@webname.com>
2012-11-06 12:31:10 +0000

    Working - but not.

--- modules/help.py
+++ modules/help.py
@@ -7,4 +7,14 @@ from maraschino.tools import requires_auth
 @app.route('/xhr/help')
 @requires_auth
 def xhr_help():
-    return render_template('/help/help-base.html')
+    return render_template('/help/help-intro.html')
+
+@app.route('/xhr/help/intro')
+@requires_auth
+def xhr_help_intro():
+    return render_template('/help/help-intro.html')
+
+@app.route('/xhr/help/modules')
+@requires_auth
+def xhr_help_modules():
+    return render_template('/help/help-modules.html')
